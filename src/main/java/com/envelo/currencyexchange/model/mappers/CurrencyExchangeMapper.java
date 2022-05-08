c4dd636cfb5b683ebf6c08dff62b6dc88571c916
Przemek Nosek <przemek.nosek@o2.pl>
2022-05-08 13:43:33 +0200

    docs: docummented mapper

--- src/main/java/com/envelo/currencyexchange/model/mappers/CurrencyExchangeMapper.java
+++ src/main/java/com/envelo/currencyexchange/model/mappers/CurrencyExchangeMapper.java
@@ -4,8 +4,20 @@ import com.envelo.currencyexchange.model.dto.AvailableCurrencyDto;
 import com.envelo.currencyexchange.model.external.ExchangeRate;
 import org.mapstruct.Mapper;
 
+/**
+ * Mapper from mapstruct, with componentModel set to "spring" to be automatically managed by Spring Container.
+ *
+ * Mapper used to do various mappings between CurrencyExchange classes.
+ */
+
 @Mapper(componentModel = "spring")
 public interface CurrencyExchangeMapper {
 
+    /**
+     * Method used to map object from {@link ExchangeRate} to {@link AvailableCurrencyDto}
+     *
+     * @param exchangeRate ExchangeRate as mapping source
+     * @return AvailableCurrencyDto as mapping target
+     */
     AvailableCurrencyDto exchangeRateToAvailableCurrencyDto(ExchangeRate exchangeRate);
 }
